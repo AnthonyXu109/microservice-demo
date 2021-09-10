a76a96a9e0bb219c9058bd8f9a25bc5046640ed9
Anthony <33390021+AnthonyXu109@users.noreply.github.com>
2021-09-10 12:49:34 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -17,49 +17,49 @@ def test_add():
 
 def test_minus():
   loop = asyncio.get_event_loop()
-  task = loop.create_task(minus(456, 123))
-  loop.run_until_complete(task)
+  task1 = loop.create_task(minus(456, 123))
+  loop.run_until_complete(task1)
   loop.close()
-  assert task.result() == {"subtraction": 333}
+  assert task1.result() == {"subtraction": 333}
 
 def test_multi():
   loop = asyncio.get_event_loop()
-  task = loop.create_task(multi(4, 4))
-  loop.run_until_complete(task)
+  task2 = loop.create_task(multi(4, 4))
+  loop.run_until_complete(task2)
   loop.close()
-  assert task.result() == {"multiplication": 16}
+  assert task2.result() == {"multiplication": 16}
 
 def test_div():
   loop = asyncio.get_event_loop()
-  task = loop.create_task(div(24, 4))
-  loop.run_until_complete(task)
+  task3 = loop.create_task(div(24, 4))
+  loop.run_until_complete(task3)
   loop.close()
-  assert task.result() == {"division": 6}
+  assert task3.result() == {"division": 6}
 
 def test_power():
   loop = asyncio.get_event_loop()
-  task = loop.create_task(power(2, 3))
-  loop.run_until_complete(task)
+  task4 = loop.create_task(power(2, 3))
+  loop.run_until_complete(task4)
   loop.close()
-  assert task.result() == {"power": 8}
+  assert task4.result() == {"power": 8}
 
 def test_sqrt():
   loop = asyncio.get_event_loop()
-  task = loop.create_task(sqrt(36))
-  loop.run_until_complete(task)
+  task5 = loop.create_task(sqrt(36))
+  loop.run_until_complete(task5)
   loop.close()
-  assert task.result() == {"square root": 6}
+  assert task5.result() == {"square root": 6}
 
 def test_mod():
   loop = asyncio.get_event_loop()
-  task = loop.create_task(mod(12, 6))
-  loop.run_until_complete(task)
+  task6 = loop.create_task(mod(12, 6))
+  loop.run_until_complete(task6)
   loop.close()
-  assert task.result() == {"modulo": 0}
+  assert task6.result() == {"modulo": 0}
 
 def test_fac():
   loop = asyncio.get_event_loop()
-  task = loop.create_task(fac(12))
-  loop.run_until_complete(task)
+  task7 = loop.create_task(fac(12))
+  loop.run_until_complete(task7)
   loop.close()
-  assert task.result() == {"factorization": '2 2 3'}
+  assert task7.result() == {"factorization": '2 2 3'}
